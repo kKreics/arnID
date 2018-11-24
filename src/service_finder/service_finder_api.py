3030c37de26b490562a28c053dca5a2f1f44654c
Illedran <nrd.nardelli@gmail.com>
2018-11-24 18:05:43 +0200

    service finder for egor + more matchers

--- src/service_finder/service_finder_api.py
+++ src/service_finder/service_finder_api.py
@@ -1,10 +1,8 @@
-import mailbox
 import email
 import re
 import base64
 import binascii
 
-mbox = mailbox.mbox('my_mails.mbox')
 verification = re.compile('(un)?subscrib|order|privacy|account|confirm|sign\s?(up|out)|verif|address|phone|auth|passw|user|name|log(in|out)|mail|\bid\b', flags=re.IGNORECASE)
 b64checker = re.compile('([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?')
 
@@ -22,29 +20,19 @@ def get_mytext(payload):
         return payload.get_payload()
     return None
 
-def parse_email(from_header):
-    return email.utils.parseaddr(from_header)[1].split('@')[-1]
-
 def get_body(mail):
     l = []
-    if mail.is_multipart():
-        for part in mail.get_payload():
-            sdss = get_mytext(part)
-            if sdss is not None:
-                l.append(sdss)
+    sdss = get_mytext(part)
+    if sdss is not None:
+        return sdss
     else:
-        if mail.get_content_maintype() == 'text':
-            sdss = get_mytext(mail)
-            if sdss is not None:
-                l.append(sdss)
-    
-    return '\n'.join(l)
-
-services = set()
-for msg in mbox:
-    f = parse_email(msg['From'])
-    body = get_body(msg)
+        return ''
+ 
+def is_signed_up(sender, body):
+    sender = email.utils.parseaddr(sender)[1].split('@')[-1]
+    body = get_body(body)
     if verification.match(body):
-        if f not in services:
-            services.add(f)
-            print(f)
+        return (sender, True)
+    else:
+        return (sender, False)
+
